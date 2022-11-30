e39f6b5ebfe9431c18467b97034204b3bb51f674
PartHivekek <117192146+PartHivekek@users.noreply.github.com>
2022-11-30 10:27:32 +0530

    awesome

--- Parthiv/parthiv_edit.py
+++ Parthiv/parthiv_edit.py
@@ -4,11 +4,11 @@ word_list=[["PINEY","PI","NEY","A mathematical expression/constant","The first 3
            ["FANFARE","FAN","FARE","A supporter of someone/something","Money paid for the journey on a public transport"],
            ["PARKOUR","PARK","OUR","an open area in a town, often with grass or trees, where people can go to walk, play and so on","of or belonging to us"],
            ["LAPTOP","LAP","TOP","one journey around a running track","the highest part or point of something"]]
-blank="_"
+blank="_ "
 a=random.choice(word_list)
 length=len(a)
 print("Rules:")
-
+guess=""
 c=a[0]
 print(a)
 print("\t 1.You will be provided with meanings of various words which connect together to form one whole meanining full word")
@@ -18,22 +18,37 @@ print("\t 4.Only full guess of each words are allowed. Partial guesses will be t
 print("Hints:")
 print("\t Hint 1 is",a[3])
 print("\t Hint 2 is",a[4])
-    
+o,p=0,0    
 for i in range(5):
     if i==0:
-        print("_"*len(c))
-        
+        print("_ "*len(c))
     t=input("Enter your guess:")
     x=t.upper()
     y,s=slice(len(a[1])),slice(len(a[1]),len(a[0]))
-    f=slice(len(a[1])-len(a[0]))
-    print(x[y])
-    print(x[s])
-    
-    if x[y]==a[1] and x[s]==a[2]:
+    f=slice(0,len(a[2]))
+    if (x[y]==a[1] and x[s]==a[2]):
         print("you win ")
         break
     elif x[y]==a[1]:
-        print(a[1],blank*(len(a[2])))    
-    elif x[s]==a[2] or x[y]==a[2]:
-        print(blank*(len(a[1])),a[2])
+        print("Hint 1 correct but 2 wrong")
+        for j in a[1]:
+          print(j,end=" ")
+        print(blank*len(a[2]))
+        o=1
+    elif x[s]==a[2] or x[f]==a[2]:
+        print("Hint 2 corrcet but 1 is wrong")
+        print(blank*len(a[1]),end=" ")
+        for j in a[2]:
+          print(j,end=" ")
+        print()
+        p=1
+    else:    
+    if  o==1 and p==1:
+        for j in a[0]:
+          print(j,end=" ")
+        print()  
+        print("you win ")
+        break
+          
+else:
+    print("U lOSE")
